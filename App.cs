a062b0538dcbdc24ca939de5c774397acff6aa60
David Ortinau <david.ortinau@microsoft.com>
2021-11-24 15:04:09 -0600

    initial commit

---
+++ App.cs
@@ -0,0 +1,19 @@
+﻿namespace MinimalMaui;
+
+public class App : Application
+{
+    public App()
+    {
+        MainPage = new ContentPage {
+            Content = new GridLayout {
+                Children = {
+                    new Label {
+                        Text = "MinimalMaui App",
+                        VerticalOptions = LayoutOptions.Center,
+                        HorizontalOptions = LayoutOptions.Center
+                    }
+                }
+            }
+        };
+    }
+}
